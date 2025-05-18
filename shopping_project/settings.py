ff65ce42bd079e655b591e166c71d505285dfe8d
jakub.korous <kubak.ko@seznam.cz>
2025-05-18 18:44:21 +0200

    Initial project setup with basic Django structure

--- shopping_project/settings.py
+++ shopping_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'shopping_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,5 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Authentication settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
 
 #django_heroku.settings(locals())
