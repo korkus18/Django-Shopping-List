2605f598e595ab3221078257a489405b4f1e0611
jakub.korous <kubak.ko@seznam.cz>
2025-05-18 18:44:21 +0200

    Add basic shopping list item model

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class ShoppingList (models.Model):
-	text = models.CharField(max_length = 40)
-	complete = models.BooleanField(default = False)
+class ItemsList(models.Model):
+	CATEGORY_CHOICES = [
+		('dairy', 'Mléčné výrobky'),
+		('vegetables', 'Ovoce a zelenina'),
+		('frozen', 'Mražené potraviny'),
+		('meal', 'Hotová jídla'),
+		('bread', 'Pečivo'),
+		('other', 'Ostatní')
+	]
+
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	itemname = models.CharField(max_length=200)
+	description = models.TextField(blank=True, null=True)
+	image = models.ImageField(upload_to='items_images/', blank=True, null=True)
+	completed = models.BooleanField(default=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+	category = models.CharField(
+		max_length=20,
+		choices=CATEGORY_CHOICES,
+		default='other'
+	)
 
 	def __str__(self):
-		return self.text
+		return self.itemname
+
+	class Meta:
+		ordering = ['-date_added']
 
